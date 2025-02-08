6ea978bc72b0e7fba5eddb01068a17857a1f72cc
kzeen <157370421+kzeen@users.noreply.github.com>
2025-02-08 23:28:29 +0200

    Create and layout new activity

---
+++ app/src/main/java/com/example/mobileassignment2/ResultActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobileassignment2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
