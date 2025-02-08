f2093706092d9a9070012804ef0d97c00cd2823d
kzeen <157370421+kzeen@users.noreply.github.com>
2025-02-09 01:26:19 +0200

    Complete second activity and refactor code

--- app/src/main/java/com/example/mobileassignment2/MainActivity.java
+++ app/src/main/java/com/example/mobileassignment2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobileassignment2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,12 +13,9 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button kz_button_10, kz_button_15, kz_button_20;
 
     private EditText kz_input_field;
-
-    private TextView kz_result_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +32,42 @@ public class MainActivity extends AppCompatActivity {
         kz_button_20 = findViewById(R.id.kz_button_20);
 
         kz_input_field = findViewById(R.id.kz_input_amount);
-
-        kz_result_view = findViewById(R.id.kz_result_20);
     }
 
     private void initializeListeners() {
         kz_button_10.setOnClickListener(v -> sameActivityResult("10"));
         kz_button_15.setOnClickListener(v -> sameActivityResult("15"));
+        kz_button_20.setOnClickListener(v -> newActivityResult());
     }
 
     private void sameActivityResult(String percentage) {
+        String kz_result = getResult(percentage);
+        if (kz_result.isEmpty()) return;
+
+        String kz_message = "Your tip is " + kz_result + "$";
+        Toast.makeText(this, kz_message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void newActivityResult() {
+        String kz_result = getResult("20");
+        if (kz_result.isEmpty()) return;
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("kz_result", kz_result);
+        startActivity(intent);
+    }
+
+    private String getResult(String percentage) {
         String kz_user_input = kz_input_field.getText().toString();
         if (invalidInput(kz_user_input)) {
             kz_input_field.setError("Please enter a valid number");
-            return;
+            return "";
         }
 
         float kz_numeric_input = Float.parseFloat(kz_user_input);
 
         float kz_percentage_decimal = Float.parseFloat(percentage) / 100;
-        String kz_result = formatNumber(kz_numeric_input * kz_percentage_decimal);
-
-        String kz_message = "Your tip is " + kz_result + "$";
-        Toast.makeText(this, kz_message, Toast.LENGTH_SHORT).show();
+        return formatNumber(kz_numeric_input * kz_percentage_decimal);
     }
 
     private boolean invalidInput(String input) {
--- app/src/main/java/com/example/mobileassignment2/ResultActivity.java
+++ app/src/main/java/com/example/mobileassignment2/ResultActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobileassignment2;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
@@ -9,5 +11,9 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        String result = getIntent().getStringExtra("kz_result");
+        TextView kz_result_view = findViewById(R.id.kz_result_20);
+        kz_result_view.setText(result);
     }
 }
